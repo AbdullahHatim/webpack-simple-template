dae38bba3693bbe04d85aa4f86a8c9e9828335b7
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-04-03 00:35:33 -0700

    Feat: Add Babel

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,48 @@
 // webpack.config.js
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: './src/template.html'
     }),
-    new FaviconsWebpackPlugin("./assets/logo.png"),
+    new FaviconsWebpackPlugin('./assets/logo.png')
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource'
       },
-    ],
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ]
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 }
